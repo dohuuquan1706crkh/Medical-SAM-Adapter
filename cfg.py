7e408d0472b7873220ea473afcf44108b1cf3fe9
nghiemkythu <49311943+nghiemkythu@users.noreply.github.com>
2025-03-18 15:36:19 +0700

    Update cfg.py

--- cfg.py
+++ cfg.py
@@ -52,6 +52,7 @@ def parse_args():
     parser.add_argument('-loss', type=str, default="BCEWithLogitsLoss", help='loss')    #DiceCELoss, BCEWithLogitsLoss
     parser.add_argument('-mid_dim', type=int, default=None , help='middle dim of adapter or the rank of lora matrix')
     parser.add_argument('-multimask_output', type=int, default=1 , help='the number of masks output for multi-class segmentation, set 2 for REFUGE dataset.')
+    parser.add_argument('-two_round', type=bool, default=False , help='two_round or not')
     parser.add_argument(
     '-data_path',
     type=str,
